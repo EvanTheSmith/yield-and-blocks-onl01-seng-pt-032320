cad9bf7ef9a95549403fb268e7b65aa9ff464a96
Evan Smith <professionalemail@zoho.com>
2020-04-03 02:36:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,4 @@
 def hello_t(array)
-  if block_given?
   i = 0
   
   while i < array.length
@@ -8,7 +7,4 @@ def hello_t(array)
   end
   
   array
-else
-  puts "Hey! No block was given!"
-end
 end
